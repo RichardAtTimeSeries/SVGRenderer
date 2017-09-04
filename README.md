d5c3d334a75868caba72aada00789382c1debe21
Gerhard Richard Edens <richard.edens@timeseries.nl>
2017-09-04 11:47:56 +0200

    Initial upload.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # SVGRenderer
 The SVGRenderer Add-On Widget allowes to create incrediable UI interface elements for the Mendix platform.
+
+## Using the SVGRenderer is very easy.
+
+You need:
+
+1. A microflow that gives a (non) persistent entity back with:
+    - An attribute containting the SVG code.
+    - A state name attribute.
+    - The attributes that the values need to be replaced inside the SVG.
+
+2. The attribute names that can be replaced inside the SVG have to be written in the SVG file as so:
+
+   ```
+   [[ attributeName ]]
+   ```
+
+The SVG rendering will render the SVG in a container. Setting the "width" and "height" attribute on the SVG tag to 100% will make the SVG responsive even in its own viewport.
+
+SVG's are extreemly powerful instruments to visualize complex user interface elements.
+
+  
---
+++ src/SVGRenderer/widget/SVGRenderer.js
@@ -0,0 +1,208 @@
+/*global logger*/
+/*
+    SVGRenderer
+    ========================
+
+    @file      : SVGRenderer.js
+    @version   : 1.0.0.
+    @author    : Gerhard Richard Edens
+    @date      : Wed, 30 Aug 2017 11:05:26 GMT
+    @copyright : TimeSeries
+    @license   : MIT
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+    "SVGRenderer/lib/jquery-1.11.2",
+    "dojo/text!SVGRenderer/widget/template/SVGRenderer.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    // Declare widget's prototype.
+    return declare("SVGRenderer.widget.SVGRenderer", [ _WidgetBase, _TemplatedMixin ], {
+        // _TemplatedMixin will create our dom node using this HTML template.
+        templateString: widgetTemplate,
+
+        // DOM elements
+        inputNodes: null,
+        colorSelectNode: null,
+        colorInputNode: null,
+        infoTextNode: null,
+
+        // Parameters configured in the Modeler.
+        mfToExecute: "",
+        messageString: "",
+        backgroundColor: "",
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _handles: null,
+        _contextObj: null,
+        _alertDiv: null,
+        _readOnly: false,
+
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            logger.debug(this.id + ".constructor");
+            this._handles = [];
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+            
+            this._updateRendering();
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            
+            var self = this;
+            
+            var guid = null;
+            if (typeof this._contextObj !== 'undefined') {
+                guid = this._contextObj.getGuid()
+            }
+
+            this._execMf(this.initialMicroflowToUse, guid, function(objs) {
+                console.log(objs[0]);
+                var obj = objs[0];
+                var stateName = obj.get(self.initialStateNameToUse);
+                var attributes = obj.getAttributes();
+                var html = obj.get(self.initialSVGContent);
+                attributes.forEach(function(attr) {
+                    html = html.split('[[ ' + attr + ' ]]').join(obj.get(attr));
+                })
+                self.content.innerHTML = html;
+            });
+            
+            this._resetSubscriptions();
+            this._updateRendering(callback); // We're passing the callback to updateRendering to be called after DOM-manipulation
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {
+          logger.debug(this.id + ".enable");
+        },
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {
+          logger.debug(this.id + ".disable");
+        },
+
+        // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {
+          logger.debug(this.id + ".resize");
+        },
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function () {
+          logger.debug(this.id + ".uninitialize");
+            // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
+        },
+
+        _execMf: function (mf, guid, cb) {
+            logger.debug(this.id + "._execMf");
+            if (mf && guid) {
+                mx.ui.action(mf, {
+                    params: {
+                        applyto: "selection",
+                        guids: [guid]
+                    },
+                    callback: lang.hitch(this, function (objs) {
+                        if (cb && typeof cb === "function") {
+                            cb(objs);
+                        }
+                    }),
+                    error: function (error) {
+                        console.debug(error.description);
+                    }
+                }, this);
+            } else {
+                mx.ui.action(mf, {
+                    params: {},
+                    callback: lang.hitch(this, function (objs) {
+                        if (cb && typeof cb === "function") {
+                            cb(objs);
+                        }
+                    }),
+                    error: function (error) {
+                        console.debug(error.description);
+                    }
+                }, this);
+            }
+        },
+
+        // Rerender the interface.
+        _updateRendering: function (callback) {
+            
+            // The callback, coming from update, needs to be executed, to let the page know it finished rendering
+            this._executeCallback(callback, "_updateRendering");
+        },
+
+        // Reset subscriptions.
+        _resetSubscriptions: function () {
+            logger.debug(this.id + "._resetSubscriptions");
+            // Release handles on previous object, if any.
+            this.unsubscribeAll();
+
+            // When a mendix object exists create subscribtions.
+            /* if (this._contextObj) {
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: lang.hitch(this, function (guid) {
+                        this._updateRendering();
+                    })
+                });
+
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this.backgroundColor,
+                    callback: lang.hitch(this, function (guid, attr, attrValue) {
+                        this._updateRendering();
+                    })
+                });
+
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: lang.hitch(this, this._handleValidation)
+                });
+            } */
+        },
+
+        _executeCallback: function (cb, from) {
+            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["SVGRenderer/widget/SVGRenderer"]);
